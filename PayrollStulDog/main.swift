bb6c7a4de334fc06e319574b58c75bd6282da184
Misha Lvovsky <mishalvovsky@Mishas-MacBook-Air.local>
2018-01-30 07:53:48 -0500

    Initial Commit

---
+++ PayrollStulDog/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PayrollStulDog
+//
+//  Created by Misha Lvovsky on 1/30/18.
+//  Copyright © 2018 No. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
