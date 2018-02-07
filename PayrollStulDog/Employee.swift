70d46f9be0b63ad8ccce6d9d69ffee1447536212
Misha Lvovsky <mishalvovsky@mishas-air.fios-router.home>
2018-02-07 02:24:28 -0500

    Made it, you know... be the assignment

--- PayrollStulDog/Employee.swift
+++ PayrollStulDog/Employee.swift
@@ -9,43 +9,139 @@
 import Foundation
 
 enum EmployeeType{
-    case manager, exec, employee, hourly, salary
+    case Employee, Salaried, Hourly, Manager, Executive
 }
 
 class Employee:CustomStringConvertible {
-    var type:EmployeeType
-    var yearsOnBoard:Int
-    var id:Int
-    var name:String
-    static var nextEmployeeID = 0
-    init(_ name:String) {
-        id = Employee.nextEmployeeID
-        Employee.nextEmployeeID += 1
-        type = .employee
-        yearsOnBoard = 0
-        self.name = name
+    private static var nextEmployeeNumber = 1
+    let lastName: String
+    let firstName: String
+    let employeeNumber: Int
+    var employeeType: EmployeeType
+    var yearsEmployeed: Int
+    
+    //weeklySalary is an abstract method
+    func weeklySalary() -> Int {
+        fatalError("It is illegal to make an Employee object")
     }
-    func workedAnotherYear() {
-        self.yearsOnBoard += 1
+    
+    func newYear() {
+        yearsEmployeed += 1
     }
-    var weeklySalary:Double {
-        fatalError("No Salary")
+    
+    func raise(amount: Int) {
+        
     }
-    var firstName:String? {
-        let lastNam = name.components(separatedBy: " ")
-        if (lastNam.count == 2){
-            return name.components(separatedBy: " ")[1]
+    
+    init(lastName: String, firstName: String, yearsEmployeed: Int, employeeType: EmployeeType) {
+        self.lastName = lastName
+        self.firstName = firstName
+        self.employeeNumber = Employee.nextEmployeeNumber
+        Employee.nextEmployeeNumber += 1
+        self.yearsEmployeed = yearsEmployeed
+        self.employeeType = employeeType
+    }
+    
+    var description: String{
+        var desc = ""
+        desc += "\(lastName), \(firstName)\n"
+        desc += "  Employee Type: \(employeeType)\n"
+        desc += "  Employee Number: \(employeeNumber)\n"
+        desc += "  YearsEmployeed: \(yearsEmployeed)\n"
+        desc += "  Weekly Salary: \(weeklySalary())\n"
+        return desc
+    }
+}
+
+class HourlyEmployee:Employee {
+    var regularHours: Int
+    var holidayHours: Int
+    var hourlyPay: Int
+    
+    func workHours(hours: Int) {
+        regularHours += hours
+    }
+    func workDoubleTimeHours(hours: Int) {
+        holidayHours += hours
+    }
+    func newWeek() {
+        regularHours = 0
+        holidayHours = 0
+    }
+    override func weeklySalary() -> Int {
+        var salary = Int(0)
+        if (regularHours > 40) {
+            salary += 40*hourlyPay
+            if (regularHours > 80) {
+                salary += hourlyPay*60
+                salary += hourlyPay*(regularHours-80)*2
+            }
+            else {
+                salary += Int(Double(hourlyPay)*(Double(regularHours) - 40)*1.5)
+            }
         }
-        else {return nil}
-    }
-    var lastName:String? {
-        let lastNam = name.components(separatedBy: " ")
-        if (lastNam.count == 2){
-            return name.components(separatedBy: " ")[1]
+        else {
+            salary = regularHours * hourlyPay
         }
-        else {return nil}
+        salary += holidayHours * 2 * hourlyPay
+        return salary
     }
-    var description: String {
-        return "Employee Name: \(name) ID: \(id), Years Hired: \(yearsOnBoard)"
+    override func raise(amount: Int) {
+        hourlyPay += amount
+    }
+    override var description:String {
+        var desc = super.description
+        desc += "  Regular Hours Worked: \(regularHours)\n"
+        desc += "  Holiday Hours Worked: \(holidayHours)\n"
+        return desc
+    }
+    init(lastName: String, firstName: String, hourlySalary: Int) {
+        holidayHours = 0
+        regularHours = 0
+        self.hourlyPay = hourlySalary
+        super.init(lastName: lastName, firstName: firstName, yearsEmployeed: 0, employeeType: .Hourly)
+        self.employeeType = .Hourly
+    }
+}
+class SalariedEmployee: Employee {
+    var yearlySalary: Int
+    
+    override func weeklySalary() -> Int {
+        return Int(yearlySalary / 52)
+    }
+    override func raise(amount: Int) {
+        yearlySalary += amount
+    }
+    init (lastName: String, firstName: String, annualSalary: Int) {
+        self.yearlySalary = annualSalary
+        super.init(lastName: lastName, firstName: firstName, yearsEmployeed: 0, employeeType: .Salaried)
+    }
+    override var description: String {
+        var desc = super.description
+        desc += "  Yearly Salary: \(yearlySalary)\n"
+        return desc
+    }
+}
+
+class ManagerEmployee: SalariedEmployee {
+    var rating: Int
+    override func weeklySalary() -> Int {
+        var weeklySalary = super.weeklySalary()
+        weeklySalary += (super.yearlySalary*(rating/10))/52
+        return weeklySalary
+    }
+    
+    func setManagerRating(newRating: Int) {
+        rating = newRating
+    }
+    init (lastName: String, firstName: String, annualSalary: Int, initialManagerRating: Int) {
+        self.rating = initialManagerRating
+        super.init(lastName: lastName, firstName: firstName, annualSalary: annualSalary)
+        self.employeeType = .Manager
+    }
+    override var description: String {
+        var desc = super.description
+        desc += "  Rating: \(rating)\n"
+        return desc
     }
 }
--- PayrollStulDog/main.swift
+++ PayrollStulDog/main.swift
@@ -5,5 +5,29 @@
 //  Created by Misha Lvovsky on 1/30/18.
 //  Copyright © 2018 No. All rights reserved.
 //
-var employee = Employee("Scrungo Beepis")
-print(employee)
+var employees = [Employee]()
+
+var s = SalariedEmployee(lastName: "Jones", firstName: "Fred", annualSalary: 35000)
+s.raise(amount:10000)
+employees.append(s)
+
+var h = HourlyEmployee(lastName: "Smith", firstName: "Phil", hourlySalary: 22)
+h.raise(amount: 3)
+h.workHours(hours:10)
+h.workHours(hours: 40)
+h.workHours(hours:35)
+h.workDoubleTimeHours(hours: 20)
+employees.append(h)
+
+var m = ManagerEmployee(lastName: "Talbot", firstName: "Henry", annualSalary: 65000, initialManagerRating: 3)
+m.raise(amount: 10000)
+m.setManagerRating(newRating: 4)
+employees.append(m)
+
+for e in employees{print(e, "\n")}
+
+print ("\n\nWeekly Payroll Check Run:\n")
+for e in employees {
+    print("\(e.lastName), \(e.firstName)")
+    print("  Check Amount: \(e.weeklySalary())\n")
+}
